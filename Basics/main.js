4645e57be29124a7547b8cb29f17ac1145482b8c
Abhishek Kumar <ak.abhishek1616@gmail.com>
2023-07-24 22:42:49 +0530

    day 4: calculator project

---
+++ Basics/main.js
@@ -0,0 +1,12 @@
+var a=[1,2,3,4]
+console.log(a.length)
+a.push(5)
+console.log(a.length)
+a.push("x")
+console.log(a.length)
+console.log(a[5])
+a[6]="p"
+console.log(a.length)
+a["x"]=7
+console.log(a.length)
+console.log(a)
